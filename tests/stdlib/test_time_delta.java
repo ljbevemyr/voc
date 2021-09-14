59a1fd26c4df1f7ee163d816f2881ad6b043afd6
Erik <36402235+erkanerk@users.noreply.github.com>
2021-09-14 09:22:36 +0200

    Create test_time_delta.java

---
+++ tests/stdlib/test_time_delta.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test_time_delta {
+
+    // private final Calculator calculator = new Calculator();
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+
+}
