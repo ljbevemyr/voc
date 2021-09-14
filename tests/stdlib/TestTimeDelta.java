ad564724bcc130d633e76ce154bf2dc64d50868c
Erik <36402235+erkanerk@users.noreply.github.com>
2021-09-14 15:10:32 +0200

    asdfs

--- tests/stdlib/TestTimeDelta.java
+++ tests/stdlib/TestTimeDelta.java
@@ -1,7 +1,24 @@
-
+import org.python.stdlib.datetime;
+import junit.extensions.TestSetup;
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class TestTimeDelta extends TestCase {
+
+    public static Test suite() {
+        TestSetup setup = new TestSetup(new TestSuite(TestTimeDelta.class)) {
+            protected void setUp() throws Exception {
+                // do your one-time setup here!
+            }
+
+            protected void tearDown() throws Exception {
+                // do your one-time tear down here!
+            }
+        };
+        return setup;
+    }
+
     public void testTrue() {
         assertTrue(true);
     }
